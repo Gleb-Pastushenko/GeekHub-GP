e99f7804ad0c89896157670986ebd9aa3c845976
Gleb <gleb.pastushenko.it@gmail.com>
2024-04-10 02:53:27 +0300

    Sell page basic view + Search complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import AboutUs from './pages/AboutUs.page';
 import Payments from './pages/Payments.page';
 import UserProfile from './pages/UserProfile.page';
 import AdsLayout from './components/AdsLayout.component';
+import Services from './pages/Services.page';
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
             <Route path="ads" element={<AdsLayout />}>
               <Route index element={<Navigate to="sell" />} />
               <Route path="sell" element={<Sell />} />
-              <Route path="services" element={<Sell />} />
+              <Route path="services" element={<Services />} />
               <Route path="vacancies" element={<Sell />} />
             </Route>
             <Route path="community" element={<Community />} />
--- frontend/src/pages/Sell.page.jsx
+++ frontend/src/pages/Sell.page.jsx
@@ -1,7 +1,62 @@
-const Sell = () => {
-  return (
-    <div>
+import { Form, Button, Row, Col, Card } from 'react-bootstrap';
 
+import { adsForSell } from '../assets/ads-for-sell'
+import { useState } from 'react';
+
+
+const Sell = () => {
+  const [searchField, setSearchField] = useState('');
+  const [adsFilteredList, setAdsFilteredList] = useState(adsForSell);
+
+  const handleSearch = e => {
+    const searchText = e.target.value.toLowerCase();
+
+    setSearchField(searchText);
+
+    setAdsFilteredList(adsForSell.filter(item => {
+      const title = item.title.toLowerCase();
+      const text = item.text.toLowerCase();
+
+      return title.includes(searchText) || text.includes(searchText);
+    }))
+  }
+
+  return (
+    <div className="pt-3">
+      <Form className="pb-4">
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Пошук"
+              value={searchField}
+              className="mr-sm-2"
+              onChange={handleSearch}
+            />
+          </Col>
+          <Col xs="auto">
+            <Button type="submit">Створити Оголошення</Button>
+          </Col>
+        </Row>
+      </Form>
+      <Row>
+        {adsFilteredList.map(itemData => (
+          <Col xs={12} md={6} lg={4} xl={3} key={itemData._id}>
+            <Card>
+              <Card.Img src={`/ads-for-sell/photos/${itemData._id}/${itemData.photos[0]}`} />
+              <Card.Body>
+                <Card.Title>
+                  {itemData.title}
+                </Card.Title>
+                <Card.Text>
+                  {itemData.text}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+
+      </Row>
     </div>
   )
 }
