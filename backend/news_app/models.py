e67ab26823be1b25c0813927cd68558c0f978f59
Gleb <gleb.pastushenko.it@gmail.com>
2024-04-13 17:02:17 +0300

    Backend Static folder set up, news model created

--- backend/news_app/models.py
+++ backend/news_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+  title = models.CharField(max_length=100)
+  text = models.TextField(null=False)
+  image = models.ImageField()
+  date = models.DateField()
+
+  def __str__(self):
+    return self.title
