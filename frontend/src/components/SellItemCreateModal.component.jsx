825e030b67bf2282af9e145cc1530ed0e77eea38
Gleb <gleb.pastushenko.it@gmail.com>
2024-04-19 21:05:44 +0300

    Sell Ad creaton form (validation added)

--- frontend/src/components/SellItemCreateModal.component.jsx
+++ frontend/src/components/SellItemCreateModal.component.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
+
 import { Modal, Card, CloseButton, Image, Form, Button, Row, Col } from 'react-bootstrap';
+
 
 const SellItemCreateModal = (_props) => {
   // Extract custom props
   const { currentUser, setIsModalShown, setIsRefreshRequired, ...props } = _props;
 
   // Form control states
+  const [formValidated, setFormValidated] = useState(false);
   const [title, setTitle] = useState('');
   const [text, setText] = useState('');
   const [selectedFiles, setSelectedFiles] = useState([]);
@@ -62,10 +65,20 @@ const SellItemCreateModal = (_props) => {
 
   // Form control handlers
   const formSubmitHandler = async (e) => {
-    e.preventDefault();
-    const result = await sendData();
-    setIsRefreshRequired(true);
+    const form = e.currentTarget;
 
+    if (!form.checkValidity()) {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log("NOT VALID!!!")
+    } else {
+      const result = await sendData();
+      setIsRefreshRequired(true);
+      resetStates();
+      setIsModalShown(false);
+    }
+
+    setFormValidated(true);
   }
 
   const titleChangeHandler = (e) => {
@@ -81,6 +94,7 @@ const SellItemCreateModal = (_props) => {
   }
 
   const closeClickHandler = () => {
+    resetStates();
     setIsModalShown(false);
   }
 
@@ -101,8 +115,9 @@ const SellItemCreateModal = (_props) => {
       </Modal.Header>
 
       <Modal.Body>
-        <Form onSubmit={formSubmitHandler} ref={formRef}>
-          <Form.Group>
+
+        <Form onSubmit={formSubmitHandler} ref={formRef} noValidate validated={formValidated}>
+          <Form.Group >
             <Form.Label>Заголовок:</Form.Label>
             <Form.Control
               required
@@ -111,7 +126,12 @@ const SellItemCreateModal = (_props) => {
               onChange={titleChangeHandler}
               name="title"
               value={title}
+              maxLength={100}
             />
+            <Form.Text>Залишилось {100 - title.length}</Form.Text>
+            <Form.Control.Feedback type="invalid">
+              "Обов'язкове поле!"
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mt-3">
             <Form.Label>Текст оголошення:</Form.Label>
@@ -123,9 +143,11 @@ const SellItemCreateModal = (_props) => {
               onChange={textChangeHandler}
               name="text"
               value={text}
+              maxLength={500}
             />
+            <Form.Text>Залишилось {500 - text.length}</Form.Text>
+            <Form.Control.Feedback type="invalid">Обов'язкове поле!</Form.Control.Feedback>
           </Form.Group>
-
           <Form.Group className="mt-3">
             <Form.Label>Додати фото:</Form.Label>
             <Form.Control
@@ -137,6 +159,7 @@ const SellItemCreateModal = (_props) => {
               name="image"
               ref={fileInputRef}
             />
+            <Form.Control.Feedback type="invalid">Завантажте хоча б одне фото!</Form.Control.Feedback>
             <Button type="submit" className="mt-3">Зберегти</Button>
           </Form.Group>
         </Form>
