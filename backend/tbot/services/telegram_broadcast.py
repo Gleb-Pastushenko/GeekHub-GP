d1b32dcbddf538d5112667d19c7377f0e8d10e68
Semsan95 <143294027+Semsan95@users.noreply.github.com>
2024-04-27 23:22:18 +0300

    Add Text and Datetime to Broadcast

--- backend/tbot/services/telegram_broadcast.py
+++ backend/tbot/services/telegram_broadcast.py
@@ -11,4 +11,8 @@ def send_news_to_users(news_id):
         image_url = news.image.url
         photo = open(f'{STATIC_URL}{image_url}', 'rb')
         
-        bot.send_photo(user.user_id, photo=photo, caption=news.title)
+        news_title = f'<b>{news.title}</b>'
+        news_date = f'<b>Подія відбудеться {news.date.strftime("%d-%m-%Y")}, о {news.date.strftime("%H:%M")}</b>'
+        news_caption = f'{news_title}\n\n{news.text}\n\n{news_date}'
+        
+        bot.send_photo(user.user_id, photo=photo, caption=news_caption, parse_mode='HTML')
