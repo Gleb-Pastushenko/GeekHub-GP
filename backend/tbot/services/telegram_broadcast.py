53f21fd6aa1118415a2ce698a716b76fbc513931
Semsan95 <143294027+Semsan95@users.noreply.github.com>
2024-04-28 22:00:22 +0000

    Upgrade TeleBot

--- backend/tbot/services/telegram_broadcast.py
+++ backend/tbot/services/telegram_broadcast.py
@@ -9,16 +9,15 @@ from backend.celery import app
 def send_news_to_users(news_id):
     users = TelegramUser.objects.all()
     news = News.objects.get(id=news_id)
-    
-    for user in users:
-        image_url = news.image.url
-        photo = open(f'{STATIC_URL}{image_url}', 'rb')
 
-        current_timezone = timezone.get_current_timezone()
-        dt_value_with_timezone = timezone.localtime(news.date, current_timezone)
-        
-        news_title = f'<b>{news.title}</b>'
-        news_date = f'<b>{news.date.strftime("%d-%m-%Y")} {dt_value_with_timezone.strftime("%H:%M")}</b>'
-        news_caption = f'{news_title}\n\n{news.text}\n\n{news_date}'
-        
-        bot.send_photo(user.user_id, photo=photo, caption=news_caption, parse_mode='HTML')
+    current_timezone = timezone.get_current_timezone()
+    dt_value_with_timezone = timezone.localtime(news.date, current_timezone)
+    
+    news_title = f'<b>{news.title}</b>'
+    news_date = f'<b>{news.date.strftime("%d-%m-%Y")} {dt_value_with_timezone.strftime("%H:%M")}</b>'
+    news_caption = f'{news_title}\n\n{news.text}\n\n{news_date}'
+
+    image_url = news.image.url
+    with open(f'{STATIC_URL}{image_url}', 'rb') as photo:
+        for user in users:
+            bot.send_photo(user.user_id, photo=photo, caption=news_caption, parse_mode='HTML')
